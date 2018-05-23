9a78c39a0069a40a035eadfdab22fdc0e5f68edd
John Tribe <crownclown67@gmail.com>
2018-05-23 19:42:20 +0200

    http code reformat

--- src/Spark/Http/HttpCode.php
+++ src/Spark/Http/HttpCode.php
@@ -11,13 +11,13 @@ namespace Spark\Http;
 
 class HttpCode {
 
-    const OK                    = 200;
-    const UNAUTHORIZED          = 401;
-    const INTERNAL_SERVER_ERROR = 500;
-    const BAD_REQUEST = 400;
+    public const OK                    = 200;
+    public const UNAUTHORIZED          = 401;
+    public const INTERNAL_SERVER_ERROR = 500;
+    public const BAD_REQUEST = 400;
 
 
-    const NOT_FOUND             = 404;
+    public const NOT_FOUND             = 404;
     public static $OK; //200
     public static $CREATED; //201
     public static $BAD_REQUEST; //400
